d60a52dc993777fbdcb469caeb571125fc7f3b87
nizaalvarado <nizayet_95@hotmail.com>
2023-12-05 20:56:00 -0600

    Primera idea del e-commerce

---
+++ tienda-Online/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.scss'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1>
+      Hola mundo!
+    </h1>
+  </React.StrictMode>,
+)
